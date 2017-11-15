0cc995ac42c4d948f60480d1ff41d89d9cb1dcfe
Arthur Pan <arthurpan24@gmail.com>
2017-11-14 18:09:27 -0800

    added information for FAQ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,61 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-function changeTable2015() {
-
-  document.getElementById("table_2015").style.display = "table";
-  document.getElementById("table_2014").style.display = "none";
-  document.getElementById("table_2013").style.display = "none";
-
-  document.getElementById("career_2015").classList.add("button_active");
-  document.getElementById("career_2014").removeAttribute("class");
-  document.getElementById("career_2013").removeAttribute("class");
-
-
-  return true;
-}
-
-function changeTable2014() {
-
-  document.getElementById("table_2015").style.display = "none";
-  document.getElementById("table_2014").style.display = "table";
-  document.getElementById("table_2013").style.display = "none";
-
-  document.getElementById("career_2014").classList.add("button_active");
-  document.getElementById("career_2015").removeAttribute("class");
-  document.getElementById("career_2013").removeAttribute("class");
-
-  return true;
-}
-
-function changeTable2013() {
-
-  document.getElementById("table_2015").style.display = "none";
-  document.getElementById("table_2014").style.display = "none";
-  document.getElementById("table_2013").style.display = "table";
-
-  document.getElementById("career_2013").classList.add("button_active");
-  document.getElementById("career_2014").removeAttribute("class");
-  document.getElementById("career_2015").removeAttribute("class");
-
-  return true;
-}
-
-function showInterviewInfo() {
-  document.getElementById("interviewInfo").style.display = "table";
-  document.getElementById("interviewInfoBtn").classList.add("button_active")
-}
-
-function showPersonalInfo() {
-
-  document.getElementById("personalInfo").style.display = "table";
-  document.getElementById("personalInfoBtn").classList.add("button_active")
-}
-
-function showEmployer() {
-  document.getElementById("employmentInfo").style.display = "inline";
-}
-
 function openFAQ(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
